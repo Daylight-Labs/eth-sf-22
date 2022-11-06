eae775fec9c426ca109a4cc4255f58da482794b3
Tassilo von Gerlach <tassi@daylight.video>
2022-11-05 18:13:12 -0700

    stuff

--- django/api/models/referral_link_signup.py
+++ django/api/models/referral_link_signup.py
@@ -5,11 +5,11 @@ class ReferralLinkSignup(TimestampedDeletableModel):
     referral_link = models.ForeignKey("ReferralLink", on_delete=models.CASCADE, related_name="link_signups")
     wallet = models.TextField()
 
-    amount_deposited = models.DecimalField(null=True, max_digits = 10, decimal_places = 2)
+    amount_deposited = models.DecimalField(null=True, blank=True, max_digits = 10, decimal_places = 2)
     action_completed_on = models.DateTimeField(null=True, blank=True)
     first_deposit_date = models.DateTimeField(null=True, blank=True)
-    twab = models.DecimalField(null=True, max_digits = 10, decimal_places = 2)
-    reward_earned = models.DecimalField(null=True, max_digits = 10, decimal_places = 2)
+    twab = models.DecimalField(null=True, blank=True, max_digits = 10, decimal_places = 2)
+    reward_earned = models.DecimalField(null=True, blank=True, max_digits = 10, decimal_places = 2)
     program_complete = models.BooleanField(null=True, default=False)
 
     def __str__(self):
